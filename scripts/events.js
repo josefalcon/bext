758fc22638b237a3139155de2561281ef6a139b7
Jose Falcon <falcon@spotify.com>
2015-09-29 22:55:02 -0400

    Move subscriber

--- scripts/events.js
+++ scripts/events.js
@@ -1,6 +1,33 @@
 import { UPDATE_TAB, REMOVE_TAB } from './actions';
 import { dispatch, initializeTabs, subscribe } from './store';
 
+const GREEN = '#2ECC40';
+const RED = '#FF4136';
+
+// order matters here, since the following subscriptions can set state.
+// listener for setting various extension attributes.
+subscribe(state => {
+  if (state.activeTab && state.tabs[state.activeTab]) {
+    let tabTitle = state.tabs[state.activeTab].title;
+    chrome.browserAction.getTitle({}, currentTitle => {
+      if (tabTitle !== currentTitle) {
+        chrome.browserAction.setTitle({title: tabTitle});
+      }
+    });
+  }
+
+  let tabCount = Object.keys(state.tabs).length.toString();
+  chrome.browserAction.getBadgeText({}, badgeText => {
+    if (tabCount !== badgeText) {
+      if (tabCount === '0') tabCount = '';
+      chrome.browserAction.setBadgeText({text: tabCount});
+    }
+  });
+
+  let color = state.isPlaying ? GREEN : RED;
+  chrome.browserAction.setBadgeBackgroundColor({color: color});
+});
+
 chrome.commands.onCommand.addListener(command => {
   dispatch({type: command});
 });
@@ -22,28 +49,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 chrome.runtime.onStartup.addListener(initializeTabs);
 chrome.runtime.onInstalled.addListener(initializeTabs);
-
-const GREEN = '#2ECC40';
-const RED = '#FF4136';
-
-// listener for setting various extension attributes.
-subscribe(state => {
-  if (state.activeTab) {
-    let tabTitle = state.tabs[state.activeTab].title;
-    chrome.browserAction.getTitle({}, currentTitle => {
-      if (tabTitle !== currentTitle) {
-        chrome.browserAction.setTitle({title: tabTitle});
-      }
-    });
-  }
-
-  let tabCount = Object.keys(state.tabs).length.toString();
-  chrome.browserAction.getBadgeText({}, badgeText => {
-    if (tabCount !== badgeText) {
-      chrome.browserAction.setBadgeText({text: tabCount});
-    }
-  });
-
-  let color = state.isPlaying ? GREEN : RED;
-  chrome.browserAction.setBadgeBackgroundColor({color: color});
-});
